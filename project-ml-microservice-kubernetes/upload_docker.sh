b7f8740d335dd654cb80aec71ea7224ced958dc8
Brandon-Canniff <brandoncanniff@gmail.com>
2020-06-03 22:00:24 -0400

    build shell script to push local to docker hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=gobrando/housing_ml_devops
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
