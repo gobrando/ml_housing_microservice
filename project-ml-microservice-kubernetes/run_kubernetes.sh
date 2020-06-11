959ecdec27473ffa9973fea8568b874228e42228
Brandon-Canniff <brandoncanniff@gmail.com>
2020-06-11 16:25:26 -0400

    update kubectl script

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,14 +8,14 @@ dockerpath=gobrando/housing_prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run app --image=$dockerpath --port=80
+kubectl create deployment app --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment app --type=LoadBalancer --port=8080 --target-port=80
+kubectl port-forward app 8000:80
 
 # Log pod name
 kubectl logs `kubectl get pods -o=name`
